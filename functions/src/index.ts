195198ce3a17ecd46b1fdb536981d8b9d909083f
mike-lvov <mike.lvov.soft@gmail.com>
2023-01-16 01:39:29 +0200

    Final changes to the cloud function for recognition

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,12 +6,44 @@ import vision from "@google-cloud/vision";
 //
 export const parseCVtoJSON = functions.https.onCall(async (data, context) => {
   const CVpath = "gs://jobquest-374812.appspot.com/CV_Anastasiia Baturkina.pdf"
-  const client = new vision.ImageAnnotatorClient();
+  const client = new vision.v1.ImageAnnotatorClient();
 
-  const textDetection = await client.documentTextDetection(CVpath);
+  const gcsSourceUri =  "gs://jobquest-374812.appspot.com/CV_Anastasiia Baturkina.pdf";
+const gcsDestinationUri = "gs://jobquest-374812.appspot.com/results/";
 
-  functions.logger.log(textDetection);
-  return {
-    textDetection
-  }
+const inputConfig = {
+  // Supported mime_types are: 'application/pdf' and 'image/tiff'
+  mimeType: "application/pdf",
+  gcsSource: {
+    uri: gcsSourceUri,
+  },
+};
+const outputConfig = {
+  gcsDestination: {
+    uri: gcsDestinationUri,
+  },
+};
+const features = [{type: "DOCUMENT_TEXT_DETECTION"}];
+const request = {
+  requests: [
+    {
+      inputConfig: inputConfig,
+      features: features,
+      outputConfig: outputConfig,
+    },
+  ],
+};
+
+// @ts-ignore
+const [operation] = await client.asyncBatchAnnotateFiles(request);
+const [filesResponse] = await operation.promise();
+const destinationUri =
+  filesResponse.responses[0].outputConfig.gcsDestination.uri;
+
+  // const textDetection = await client.documentTextDetection(CVpath);
+
+  // functions.logger.log(textDetection);
+  // return {
+  //   textDetection
+  // }
 });
