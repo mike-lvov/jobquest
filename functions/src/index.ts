c6c9dbb8f26d1bbdb8329fdcecc42254e80a6cbe
mike-lvov <mike.lvov.soft@gmail.com>
2023-01-16 14:50:58 +0200

    Reworks file savings and retrieval

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -21,10 +21,10 @@ export const parseCVtoJSON = functions.https.onCall(async (data, context) => {
   
   const client = new vision.v1.ImageAnnotatorClient();
 
-  const newJSONFileName = `${uuidv4()}.json`
+  const newJSONFileNamePrefix = `${uuidv4()}`
 
   const gcsSourceUri =  data.cvFilePath
-  const gcsDestinationUri = `gs://jobquest-374812.appspot.com/results/${newJSONFileName}`;
+  const gcsDestinationUri = `gs://jobquest-374812.appspot.com/results/${newJSONFileNamePrefix}`;
 
   const inputConfig = {
     // Supported mime_types are: 'application/pdf' and 'image/tiff'
@@ -56,13 +56,29 @@ export const parseCVtoJSON = functions.https.onCall(async (data, context) => {
     filesResponse.responses[0].outputConfig.gcsDestination.uri;
 
   const storage = admin.app().storage()
-  const filePath = storage.bucket("gs://jobquest-374812.appspot.com").file(`results/${newJSONFileName}`);
-  const fileObject = await filePath.download();
-  const dataObject = JSON.parse(fileObject.toString());
-  const finalString = dataObject.responses.reduce((accumulator: any, currentValue: any) => accumulator + currentValue.fullTextAnnotation.text, "");
-  console.log(finalString);
+  const [files] = await storage.bucket("gs://jobquest-374812.appspot.com").getFiles({ prefix: `results/${newJSONFileNamePrefix}` })
+
+  let responseString = '';
+  for await (const file of files) {
+    const fileObject = await file.download();
+    const dataObject = JSON.parse(fileObject.toString());
+    const finalString = dataObject.responses.reduce((accumulator: any, currentValue: any) => accumulator + currentValue.fullTextAnnotation.text, "");
+    responseString += finalString
+  }
+  // const [files] = await bucket.getFiles({ prefix: 'userData/some-file-'});
+  //  files.forEach(async (file: any) => {
+  //     const fileObject = await file.download();
+  //     const dataObject = JSON.parse(fileObject.toString());
+  //     const finalString = dataObject.responses.reduce((accumulator: any, currentValue: any) => accumulator + currentValue.fullTextAnnotation.text, "");
+  //  });
+
+
+  // const fileObject = await filePath.download();
+  // const dataObject = JSON.parse(fileObject.toString());
+  // const finalString = dataObject.responses.reduce((accumulator: any, currentValue: any) => accumulator + currentValue.fullTextAnnotation.text, "");
+  // console.log(finalString);
   return {
     destinationUri,
-    finalString
+    responseString
   }
 });
